cb4f67e8947573d37497fabe2d5e2d13c9e08e4e
James Mayell <jplam14@gmail.com>
2016-11-28 23:05:03 +0000

    Adding a few comments to code

--- js/mapFunctions.js
+++ js/mapFunctions.js
@@ -1,8 +1,18 @@
+/**
+ * initMap()
+ *
+ * this function controls the Google map on the page, using an array containing
+ * styles to make it look cool, and coordinates and a custom logo image to
+ * show the map marker in the correct location.
+ *
+ */
 
 function initMap() {
 
+  //Set the latitude and longitude coords
   var uluru = {lat: 52.955540, lng: -1.143263};
 
+  //Setup the google map using a few options and our style array
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: uluru,
@@ -12,6 +22,7 @@ function initMap() {
     styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
   });
 
+  //Set the map marker custom logo and positioning
   var marker = new google.maps.Marker({
     position: uluru,
     map: map,
--- js/pageFunctions.js
+++ js/pageFunctions.js
@@ -1,12 +1,21 @@
+/**
+ * pageFunctions.js
+ *
+ * A few functions to aid with the display of the page on desktop and mobile.
+ * decided to use js for these because hover is not supported on mobile devices
+ * and for the rounded blocks we want one to be highlighted by default
+ *
+ */
+
 jQuery(document).ready(function($) {
 
-$.mobile.ajaxEnabled=false;
-
+//Function to apply hover state to rounded blocks
 $('.rounded-block').hover(function() {
   $('.rounded-block').removeClass('active');
   $(this).addClass('active');
 });
 
+//function to show case study content on mobile using jQuery mobile tap function
 $('.case-study-content').on("tap", function() {
   $('.case-study-content').removeClass('active');
   $(this).addClass('active');
